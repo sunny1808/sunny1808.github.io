6314aaee28ea01985243d221b21bd3b07f0297fc
sunny-agsft <schakole@agsft.com>
2019-01-14 08:43:32 +0530

    PROD BUILD: 14-9-18

---
+++ assets/js/app-sidebar.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  console.log("sidebar js loaded!!");
+
+  // sidebar collapse 
+  $('#sidebarCollapse').on('click', function () {
+    $('#sidebar, #content').toggleClass('active');
+    $('.collapse.in').toggleClass('in');
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  });
+});
